a02a13f878b6b1efd54712c1e17d9dfdfb017ca9
Yohrman Daniel Garcia Vergara <daniel.garciav@vivaaerobus.com>
2022-08-12 11:19:54 -0500

    first commit

---
+++ app/Http/Livewire/FW.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class FW extends Component
+{
+    public function render()
+    {
+        return view('livewire.f-w');
+    }
+}
