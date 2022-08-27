7af5ddadb7b601c4819b0e5c0257d9d4219effaa
Yohrman Daniel Garcia Vergara <daniel.garciav@vivaaerobus.com>
2022-08-26 23:02:58 -0500

    GDL

--- app/Http/Livewire/GDL.php
+++ app/Http/Livewire/GDL.php
@@ -2,17 +2,56 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\LegMain;
 use Livewire\Component;
 use Illuminate\Support\Facades\Auth;
 
 class GDL extends Component
 {
+    public function __construct()
+    {
+        $this->baseUri  = env('BASE_URI');
+        $this->AIMSUser = env('AIMS_USER');
+        $this->AIMSPass = env('AIMS_Pass');
+    }
+
     public function render()
     {
         if (Auth::user()->rol == 'admin' || Auth::user()->rol == 'jt' || Auth::user()->rol == 'ccv' || Auth::user()->rol == 'gdl') {
+            try{
+                $OQAD = LegMain::select([\DB::raw("VIV.dbo.fn_ToAimsDate(CONVERT(datetime, GETDATE() + 1, 103)) DATE")])->first();
+
+                $this->flights = \DB::table('AIMSPROD.VIV.dbo.LEGMAIN AS FL')
+                    ->select(['FL.REG'
+                        , \DB::raw("CASE WHEN (SELECT TOP 1 FL2.FLT FROM AIMSPROD.VIV.dbo.LEGMAIN FL2 WITH (NOLOCK) WHERE FL2.DAY BETWEEN ".$OQAD->DATE." - 1 AND ".$OQAD->DATE." + 1 AND FL2.REG = FL.REG AND FL2.ARR = 'GDL' AND ((FL2.DAY = FL.DAY AND FL2.STD < FL.STD) OR (FL2.DAY < FL.DAY)) AND CARRIER = 1 AND CANCELLED = 0 ORDER BY DAY DESC, STD DESC) IS NULL THEN 'HGR' ELSE (SELECT TOP 1 CAST(FL2.FLT AS varchar) FROM AIMSPROD.VIV.dbo.LEGMAIN FL2 WITH (NOLOCK) WHERE FL2.DAY BETWEEN ".$OQAD->DATE." - 1 AND ".$OQAD->DATE." + 1 AND FL2.REG = FL.REG AND FL2.ARR = 'GDL' AND ((FL2.DAY = FL.DAY AND FL2.STD < FL.STD) OR (FL2.DAY < FL.DAY)) AND CARRIER = 1 AND CANCELLED = 0 ORDER BY DAY DESC, STD DESC) END 'Llegada'")
+                        , \DB::raw("CASE WHEN (SELECT TOP 1 FL2.DEP FROM AIMSPROD.VIV.dbo.LEGMAIN FL2 WITH (NOLOCK) WHERE FL2.DAY BETWEEN ".$OQAD->DATE." - 1 AND ".$OQAD->DATE." + 1 AND FL2.REG = FL.REG AND FL2.ARR = 'GDL' AND ((FL2.DAY = FL.DAY AND FL2.STD < FL.STD) OR (FL2.DAY < FL.DAY)) AND CARRIER = 1 AND CANCELLED = 0 ORDER BY DAY DESC, STD DESC) IS NULL THEN 'HGR' ELSE (SELECT TOP 1 CAST(FL2.DEP AS varchar) FROM AIMSPROD.VIV.dbo.LEGMAIN FL2 WITH (NOLOCK) WHERE FL2.DAY BETWEEN ".$OQAD->DATE." - 1 AND ".$OQAD->DATE." + 1 AND FL2.REG = FL.REG AND FL2.ARR = 'GDL' AND ((FL2.DAY = FL.DAY AND FL2.STD < FL.STD) OR (FL2.DAY < FL.DAY)) AND CARRIER = 1 AND CANCELLED = 0 ORDER BY DAY DESC, STD DESC) END 'Origen'")
+                        , \DB::raw("(SELECT TOP 1 CONVERT(VARCHAR(16),VIV_CORE.dbo.GetStartTimeStrViv_V5(FL2.DAY, FL2.STA, FL2.ARR), 120) FROM AIMSPROD.VIV.dbo.LEGMAIN FL2 WITH (NOLOCK) WHERE FL2.DAY BETWEEN ".$OQAD->DATE." - 1 AND ".$OQAD->DATE." + 1 AND FL2.REG = FL.REG AND FL2.ARR = 'GDL' AND ((FL2.DAY = FL.DAY AND FL2.STD < FL.STD) OR (FL2.DAY < FL.DAY)) AND CARRIER = 1 AND CANCELLED = 0 ORDER BY DAY DESC, STD DESC) 'STA'")
+                        , 'FL.FLT'
+                        , 'FL.ARR'
+                        , \DB::raw('CONVERT(VARCHAR(16),VIV_CORE.dbo.GetStartTimeStrViv_V5(FL.DAY, FL.STD, FL.DEP), 120) STD')
+                    ])
+                    //->where('STD', '>', 300)
+                    ->where('FL.DEP', '=', 'GDL')
+                    ->where('FL.CARRIER', '=', 1)
+                    ->where('FL.CANCELLED', '=', 0)
+                    ->where(function($query) use ($OQAD) {
+                        $query->where('FL.DAY', '<', $OQAD->DATE + 1)
+                            ->orWhere('FL.STA', '<=', 600);
+                    })
+                    ->whereBetween('FL.DAY', [$OQAD->DATE, $OQAD->DATE + 1])
+                    ->where(\DB::raw("CONVERT(VARCHAR(16),VIV_CORE.dbo.GetStartTimeStrViv_V5(FL.DAY, FL.STD, FL.DEP), 120)"), '>=', \DB::raw("CAST(GETDATE() + 1 AS DATE)"))
+                    ->orderBy('FL.DAY')
+                    ->orderBy('FL.STD')
+                    ->get();
+
+            }catch(Exception $e){
+                return view('livewire.redirect');
+            }
+
             return view('livewire.g-d-l');
         }
 
         return view('livewire.redirect');
     }
+
 }
--- resources/views/livewire/g-d-l.blade.php
+++ resources/views/livewire/g-d-l.blade.php
@@ -1,12 +1,72 @@
 <x-slot name="header">
     <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-        {{ __('GDL') }}
+        {{ __('FW') }}
     </h2>
 </x-slot>
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-            Esto es GDL
+            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="py-3 px-6">
+                            Matrícula
+                        </th>
+                        <th scope="col" class="py-3 px-6">
+                            Llegada
+                        </th>
+                        <th scope="col" class="py-3 px-6">
+                            Origen
+                        </th>
+                        <th scope="col" class="py-3 px-6">
+                            STA
+                        </th>
+                        <th scope="col" class="py-3 px-6">
+                            Salida
+                        </th>
+                        <th scope="col" class="py-3 px-6">
+                            Destino
+                        </th>
+                        <th scope="col" class="py-3 px-6">
+                            STD
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach ($flights as $flight)
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <td class="py-4 px-6">
+                            {{ $flight->REG }}
+                            <input type="hidden" name="REG[]" value="{{ $flight->REG }}" />
+                        </td>
+                        <td class="py-4 px-6">
+                            {{ $flight->Llegada }}
+                            <input type="hidden" name="Llegada[]" value="{{ $flight->Llegada }}" />
+                        </td>
+                        <td class="py-4 px-6">
+                            {{ $flight->Origen }}
+                            <input type="hidden" name="Origen[]" value="{{ $flight->Origen }}" />
+                        </td>
+                        <td class="py-4 px-6">
+                            {{ $flight->STA }}
+                            <input type="hidden" name="STA[]" value="{{ $flight->STA }}" />
+                        </td>
+                        <td class="py-4 px-6">
+                            {{ $flight->FLT }}
+                            <input type="hidden" name="FLT[]" value="{{ $flight->FLT }}" />
+                        </td>
+                        <td class="py-4 px-6">
+                            {{ $flight->ARR }}
+                            <input type="hidden" name="ARR[]" value="{{ $flight->ARR }}" />
+                        </td>
+                        <td class="py-4 px-6">
+                            {{ $flight->STD }}
+                            <input type="hidden" name="STD[]" value="{{ $flight->STD }}" />
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
