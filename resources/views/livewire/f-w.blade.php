0be11eda95dfe9f091b6d2280ac8a174502123d6
Yohrman Daniel Garcia Vergara <daniel.garciav@vivaaerobus.com>
2022-09-28 22:59:48 -0500

    FW

--- resources/views/livewire/f-w.blade.php
+++ resources/views/livewire/f-w.blade.php
@@ -6,7 +6,7 @@
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-            <form method="POST" onSubmit="if(!confirm('¿Realmente desea enviar el correo?')){return false;}" action="{{ url("FW/SendEmail") }}" validate>
+            <form method="POST" onSubmit="if(!confirm('¿Realmente desea enviar el correo FW?')){return false;}" action="{{ url("FW/SendEmail") }}" validate>
                 {{ csrf_field() }}
                 {{ method_field('PUT') }}
                 <div class="overflow-x-auto relative">
